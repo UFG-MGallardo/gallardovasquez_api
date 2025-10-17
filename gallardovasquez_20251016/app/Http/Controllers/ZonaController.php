bef5569298bc89a980576975734f767b7b5938df
Manuel Sebastian Gallardo Vasquez <ia.gallardo520@ufg.edu.sv>
2025-10-16 20:00:33 -0600

    Clase16102025

---
+++ gallardovasquez_20251016/app/Http/Controllers/ZonaController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ZonaController extends Controller
+{
+    //
+
+    public function obtenerZonas(){
+        $Zona = new Zona();
+        $valores = $Zona::all();
+        $respuesta = [
+            "success" => true,
+            "msg" => "Valores devueltos por el EndPoint",
+            "data" => $valores,
+            "error" => "",
+            "total" => sizeof($valores)
+        ];
+        return response()->json($respuesta,200);
+    }
+
+    public function obtenerZona(){
+        $Zona = new Zona();
+        $valores = $Zona->where('id_zona', $idzona)->get();
+        $respuesta = [
+            "success" => true,
+            "msg" => "Valores devueltos por el EndPoint",
+            "data" => $valores,
+            "error" => "",
+            "total" => sizeof($valores)
+        ];
+        return response()->json($respuesta,200);
+    }
+}
